ac7b3f64e407743fd5e4bba9d4f0a108772b5603
MrFrozeN <bartekstafiej2@gmail.com>
2020-09-17 13:16:13 +0200

    refactor detabse

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_105745) do
+ActiveRecord::Schema.define(version: 2020_09_11_100552) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_09_10_105745) do
     t.integer "role"
   end
 
+  add_foreign_key "posts", "users"
 end
